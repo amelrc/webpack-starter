1ddb57c495e72014a90c5209d22e210b5f28ad6e
Amel Ramirez <amelrc@gmail.com>
2019-09-09 15:01:32 +0200

    check funtionality, some styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,28 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click', () => {
+    console.log('button clicked');
+
+    
+});
+
+let checkboxes = document.querySelectorAll('.checkbox');
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', (el) => checked(el));
+});
+
+const checked = el => {
+    if (el.target.checked === true){
+        //display only checked
+        console.log('checked');
+        // el.style.display = "none";
+    }
+};
+
+
