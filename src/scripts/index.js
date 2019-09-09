34573e141b62bfbb09bda509756cbf28a3a35516
Amel Ramirez <amelrc@gmail.com>
2019-09-09 16:56:48 +0200

    no parameter needed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,17 +12,17 @@ button.addEventListener('click', () => {
 let checkboxes = document.querySelectorAll('.checkbox');
 
 checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('click', (el) => checked(el));
+    checkbox.addEventListener('click', () => checked());
 });
 
-const checked = el => {
+const checked = () => {
     let checked = 0;
     for (let i = 0; i < checkboxes.length; i++) {
         if(checkboxes[i].checked) {
             checked += 1;
         }
         if (checked > 4){
-            alert('Four Dips please');
+            alert('Four Dips Please');
             checkboxes[i].checked = false;
             return false;
         }  
