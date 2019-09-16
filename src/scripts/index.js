ed184bf311e8a2950746292a8fbe6f76ea0fe0e3
Amel Ramirez <amelrc@gmail.com>
2019-09-16 18:36:26 +0200

    cleaning functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,31 +2,27 @@ import '../styles/index.scss';
 
 console.log('webpack starterkit');
 
-
+let checkboxes = document.querySelectorAll('.checkbox');
 const button = document.querySelector('.button');
 
-button.addEventListener('click', () => {
-    console.log('button clicked');
-});
-
-let checkboxes = document.querySelectorAll('.checkbox');
-
 checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('click', () => checked());
+    checkbox.addEventListener('click', (el) => {
+        const classTarget = el.target.parentNode.children[0].classList;
+        let checked = 0;
+        (el.target.checked) ? classTarget.remove('hidden') : classTarget.add('hidden');
+        checkboxes.forEach(checkbox => (checkbox.checked) ? checked ++ : '');
+        (checked > 4) ? (
+            el.target.checked = false,
+            classTarget.add('hidden')) :
+            '';
+    });
 });
 
-const checked = () => {
-    let checked = 0;
-    for (let i = 0; i < checkboxes.length; i++) {
-        if(checkboxes[i].checked) {
-            checked += 1;
-        }
-        if (checked > 4){
-            alert('Four Dips Please');
-            checkboxes[i].checked = false;
-            return false;
-        }  
+button.addEventListener('click', () => checkboxes.forEach(checkbox => (checkbox.checked) ? labelChecker() : ''));
+
+const labelChecker = () => {
+    const labels = document.querySelectorAll('.label');
+    for(let label of labels) {
+        (label.lastElementChild.checked == false) ? label.parentElement.classList.add('hidden') : '';
     }
 };
-
-
